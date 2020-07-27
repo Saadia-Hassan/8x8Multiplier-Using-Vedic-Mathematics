d51ca2e5832dd68f76775f230c842b2a668a17d5
Saadia Hassan <53932260+Saadia-Hassan@users.noreply.github.com>
2020-07-27 23:58:43 +0530

    Add files via upload

---
+++ tb_booth8x8.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:33:10 07/03/2020 
+// Design Name: 
+// Module Name:    tb_booth8x8 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module tb_booth8x8;
+reg clk, start;
+reg [7:0] a, b;
+wire [15:0] ab;
+wire busy;
+booth_8x8 multiplier1(ab, busy, a, b, clk, start);
+initial begin
+clk = 0;
+$display("first example: a = 3 b = 17");
+a = 3; b = 17; start = 1; #50 start = 0;
+#80 $display("first example done");
+$display("second example: a = 7 b = 7");
+a = 7; b = 7; start = 1; #50 start = 0;
+#80 $display("second example done");
+$finish;
+end
+always #5 clk = !clk;
+always @(posedge clk) $strobe("ab: %d busy: %d at time=%t", ab, busy, $stime);
+endmodule
+
+
+
+
+
